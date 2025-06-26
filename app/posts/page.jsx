ef8365dbe2850b6ba6b73e3b3aafdf2b45b6a129
foobarsik <olgaraat@gmail.com>
2025-06-26 18:31:17 +0200

    feat: update blog title

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -3,7 +3,7 @@ import { PostCard } from 'nextra-theme-blog'
 import { getPosts, getTags } from './get-posts'
 
 export const metadata = {
-    title: 'import signals from \'my-brain\''
+    title: 'import threads from \'my-brain\''
 }
 
 export default async function PostsPage() {
