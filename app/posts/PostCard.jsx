35b2aff312567d845328c1b8b331d44808491d09
foobarsik <olgaraat@gmail.com>
2025-07-16 03:15:37 +0200

    ui: remove date from post card, it doesn't bring much useful right now

--- app/posts/PostCard.jsx
+++ app/posts/PostCard.jsx
@@ -5,24 +5,19 @@ export default function PostCard({post}) {
     const {title, description, date, cover} = frontMatter || {};
 
     return (
-        <Link href={route} className="block overflow-hidden my-11 group no-underline ">
+        <Link href={route} className="block overflow-hidden my-11 group no-underline">
             {cover && (
-                <div className="relative rounded-[10px] overflow-hidden">
+                <div className="relative rounded-[10px] overflow-hidden mb-4">
                     <img
                         src={cover}
                         alt={title}
                         className="w-full h-[150px] sm:h-[222px] object-cover block p-0 m-0 transition-transform group-hover:scale-[1.1]"
                     />
-                    {date && (
-                        <div className="absolute bottom-2 right-2 bg-white/80 text-xs text-black px-2 py-1 rounded">
-                            {new Date(date).toDateString()}
-                        </div>
-                    )}
                 </div>
             )}
             <div>
-                <h3 className="mt-4 mb-1 post-card-title">{title}</h3>
-                <p className="x:mb-2 x:dark:text-gray-400 x:text-gray-600">{description}</p>
+                <h3 className="post-card-title">{title}</h3>
+                <p className="mt-1 x:mb-2 x:dark:text-gray-400 x:text-gray-600">{description}</p>
             </div>
         </Link>
     );
