c8fcac48d76745f59f94289732d94ac398e0f5ca
foobarsik <olgaraat@gmail.com>
2026-03-26 12:26:08 +0000

    Remove unused tags feature and add muted image style.
    Commented out unused tags-related code in `PostsPage` for clarity and to reduce processing. Introduced a new `.post-card-image-muted` style with a high grayscale filter for muted image display.

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
 import { getPosts } from './utils/get-posts'
-import {getTags} from "./utils/get-tags";
 import PostCard from './PostCard'
 
 export const metadata = {
@@ -8,16 +6,18 @@ export const metadata = {
 }
 
 export default async function PostsPage() {
-    const tags = await getTags()
+    // const tags = await getTags()
     const posts = await getPosts()
-    const allTags = Object.create(null)
+    // const allTags = Object.create(null)
 
-    for (const tag of tags) {
-        allTags[tag] ??= 0
-        allTags[tag] += 1
-    }
+    // for (const tag of tags) {
+    //     allTags[tag] ??= 0
+    //     allTags[tag] += 1
+    // }
+
     return (
         <div data-pagefind-ignore="all">
+            {/*
             <div className="not-prose flex flex-wrap gap-2 mb-14">
                 {Object.entries(allTags).map(([tag, count]) => (
                     <Link key={tag} href={`/tags/${tag}`} className="nextra-tag">
@@ -25,6 +25,7 @@ export default async function PostsPage() {
                     </Link>
                 ))}
             </div>
+            */}
             {posts.map(post => (
                 <PostCard key={post.route} post={post} />
             ))}
